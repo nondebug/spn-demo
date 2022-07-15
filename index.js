4d835f3357c026e863ca3bb9bd4c727f748e5a51
pr3cast <pr3castdragon@gmail.com>
2022-07-15 01:05:37 -0400

    scriptprocessor gain vis works

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const lg = async (x) => console.log(await x);
 let isRecording = false,
     isMonitoring = false;
 
+let vizVar = true;
+
+const analyserNode = new AnalyserNode(context, {
+    fftSize: 128,
+});
+
 // AUDIO
 
 const medianStart = new GainNode(context);
 const medianEnd = new GainNode(context);
+
+let dataArr = [],
+    currData = [];
 
 // SETUP MONITOR
 
@@ -21,38 +30,165 @@ const updateMonitorGain = (enabled) => {
     monitorNode.gain.setTargetAtTime(newVal, context.currentTime, 0.01);
 };
 
-// SETUP VISUALIZER: LIVE
+// Controls
 
-// SETUP VIZ: RECORDING
+const monitorButton = document.querySelector("#monitor"),
+    monitorText = monitorButton.querySelector("span");
+
+monitorButton.addEventListener("click", (e) => {
+    isMonitoring = !isMonitoring;
+
+    updateMonitorGain(isMonitoring);
+
+    monitorText.innerHTML = isMonitoring ? "off" : "on";
+});
+
+// SETUP VISUALIZER
+
+// Live Script Vis
+
+const visualizer = document.querySelector("#live-canvas");
+
+let dx = 0;
+
+const drawLiveVis = () => {
+    requestAnimationFrame(drawLiveVis);
+
+    if (!vizVar) return;
+    if (!currData) return;
+
+    const bufferLength = currData.length;
+
+    const width = visualizer.width,
+        height = visualizer.height;
+
+    const canvasContext = visualizer.getContext("2d");
+
+    // save buffer as data
+
+    let mode = currData.reduce((e, a) => a + e, 0) / currData.length;
+
+    let loudness = mode;
+
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(dx, ((1 - loudness) * height) / 2, 1, 1);
+
+    canvasContext.fillStyle = "black";
+
+    canvasContext.fillRect(
+        dx,
+        ((1 - loudness) * height) / 2,
+        1,
+        loudness * height,
+    );
+
+    if (dx < width - 1) {
+        dx++;
+    } else {
+        dx = 0;
+    }
+
+    canvasContext.fillStyle = "rgba(255,255,255,.8)";
+    canvasContext.fillRect(dx + 1, 0, 1, height);
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(dx - 1, ((1 - loudness) * height) / 2, 1, 1);
+};
+
+drawLiveVis();
+
+// Live Analyzer Vis
+
+const anaVis = document.querySelector("#analyzer-vis");
+function drawAnalyserVis() {
+    requestAnimationFrame(drawAnalyserVis);
+
+    if (!vizVar) return;
+
+    const bufferLength = analyserNode.frequencyBinCount;
+
+    const width = anaVis.width,
+        height = anaVis.height,
+        barWidth = width / bufferLength;
+
+    const canvasContext = anaVis.getContext("2d");
+    canvasContext.clearRect(0, 0, width, height);
+
+    // save buffer as data
+    const dataArray = new Uint8Array(bufferLength);
+    analyserNode.getByteFrequencyData(dataArray);
+
+    // console.log("anavis" + dataArray);
+
+    dataArray.forEach((item, index) => {
+        const y = (item / 255) * height * 0.9;
+        const x = barWidth * index;
+
+        canvasContext.fillStyle = `hsl(${(y / height) * 2 * 200}, 100%, 50%)`;
+        canvasContext.fillRect(x, height - y, barWidth, y);
+    });
+}
+
+drawAnalyserVis();
+
+// Controls
+
+const vizToggle = document.querySelector("#viz-toggle");
+
+vizToggle.addEventListener("click", (e) => {
+    vizVar = !vizVar;
+
+    vizToggle.querySelector("span").innerHTML = vizVar ? "Pause" : "Play";
+});
+
+// RECORDING
+
+// Recording Controls
+
+const recordButton = document.querySelector("#record"),
+    recordText = recordButton.querySelector("span"),
+    player = document.querySelector("#player");
+
+recordButton.addEventListener("click", (e) => {
+    isRecording = !isRecording;
+
+    recordText.innerHTML = isRecording ? "Stop" : "Start";
+
+    if (isRecording) {
+    } else {
+        document.querySelector("#data-len").innerHTML = dataArr.length;
+        player.src = URL.createObjectURL(new Blob(dataArr));
+    }
+});
 
 // Get mic and call func
 
-// SETUP MIC
+// SETUP
+
+// Mic
+
+const processCallback = (stream) => {
+    const micSource = context.createMediaStreamSource(stream);
+
+    micSource.connect(medianStart);
+
+    const processor = context.createScriptProcessor(256, 1, 1);
+
+    processor.onaudioprocess = function (e) {
+        currData = e.inputBuffer.getChannelData(0);
+
+        if (isRecording) {
+            dataArr.push(currData);
+
+            lg(dataArr);
+        }
+    };
+
+    return processor;
+};
 
 const setMicGetProcessorNode = async () => {
-    const dataArr = [];
-
     // Triggered when mic is selected
-    const setupProcessor = (stream) => {
-        const micSource = context.createMediaStreamSource(stream);
-
-        micSource.connect(medianStart);
-
-        const processor = context.createScriptProcessor(1024, 1, 1);
-
-        processor.onaudioprocess = function (e) {
-            if (isRecording) {
-                const data = e.inputBuffer.getChannelData(0);
-                dataArr.push(...data);
-
-                console.log(
-                    `${dataArr.length}, ${dataArr[dataArr.length - 1]}`,
-                );
-            }
-        };
-
-        return processor;
-    };
+    const setupProcessor = processCallback;
 
     const at20 =
         "7c142e20af72ddc0bb42359c74b7693031ac4cf27870749f0f53553d15fd6c8f";
@@ -70,7 +206,7 @@ const setMicGetProcessorNode = async () => {
         .then(setupProcessor);
 };
 
-// FINAL CHAIN
+// Initializer
 
 async function setupContext() {
     const processor = await setMicGetProcessorNode(medianStart);
@@ -81,39 +217,20 @@ async function setupContext() {
 
     // Mic to proces
 
-    medianStart.connect(medianEnd);
-
-    medianEnd.connect(monitorNode).connect(context.destination);
+    medianStart
+        .connect(medianEnd)
+        .connect(monitorNode)
+        .connect(context.destination);
 
     // Separate out for SP. Thru SP to dest only doesn't output any audio
     // https://github.com/WebAudio/web-audio-api/issues/345
-    medianEnd.connect(processor).connect(context.destination);
-    // processor.connect(context.destination);
+    medianEnd
+        .connect(analyserNode)
+        .connect(processor)
+        .connect(context.destination);
 }
 
 setupContext();
-
-// CONTROLS
-
-const recordButton = document.querySelector("#record"),
-    recordText = recordButton.querySelector("span");
-
-recordButton.addEventListener("click", (e) => {
-    isRecording = !isRecording;
-
-    recordText.innerHTML = isRecording ? "Stop" : "Start";
-});
-
-const monitorButton = document.querySelector("#monitor"),
-    monitorText = monitorButton.querySelector("span");
-
-monitorButton.addEventListener("click", (e) => {
-    isMonitoring = !isMonitoring;
-
-    updateMonitorGain(isMonitoring);
-
-    monitorText.innerHTML = isMonitoring ? "off" : "on";
-});
 
 context.addEventListener("statechange", (e) => {
     document.querySelector("#ctx-status").innerHTML = context.state;
